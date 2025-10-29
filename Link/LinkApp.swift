b0d0a64ed9a477232e5a0e7c12978c5e5eec95eb
Gustavo Luis Sore <sore.gustavo@gmail.com>
2025-10-29 15:50:50 -0300

    Initial Commit

---
+++ Link/LinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkApp.swift
+//  Link
+//
+//  Created by Gustavo Soré on 29/10/25.
+//
+
+import SwiftUI
+
+@main
+struct LinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LinkTests/LinkTests.swift
@@ -0,0 +1,16 @@
+//
+//  LinkTests.swift
+//  LinkTests
+//
+//  Created by Gustavo Soré on 29/10/25.
+//
+
+import Testing
+
+struct LinkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
